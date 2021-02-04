6d1848d9de26d7a0eac8367ea4c635dc7341b56d
wlwl0219 <wlwl0219@gmail.com>
2021-02-04 23:22:17 +0900

    Create Quiz

---
+++ src/components/QuestionCard.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { AnswerObject } from "../App";
+
+type Props = {
+  question: string;
+  answers: string[];
+  callback: (e: React.MouseEvent<HTMLButtonElement>) => void;
+  userAnswer: AnswerObject | undefined;
+  questionNr: number;
+  totalQuestions: number;
+};
+
+const QuestionCard: React.FC<Props> = ({
+  question,
+  answers,
+  callback,
+  userAnswer,
+  questionNr,
+  totalQuestions,
+}) => (
+  <div>
+    <p className="number">
+      Question: {questionNr} / {totalQuestions}
+    </p>
+    {/* InnerHTML 용도 */}
+    <p dangerouslySetInnerHTML={{ __html: question }} />
+    <div>
+      {answers.map(answer => (
+        <div key={answer}>
+          {/* userAnswer ? true : false 삼항연산자 혹은 !! */}
+          <button disabled={!!userAnswer} value={answer} onClick={callback}>
+            <span dangerouslySetInnerHTML={{ __html: answer }} />
+          </button>
+        </div>
+      ))}
+    </div>
+  </div>
+);
+
+export default QuestionCard;
